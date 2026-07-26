8a59d09024b7fba6a7bce619e178c8c0d8e87c7d
ThangNN99 <ngocthang2710.1999@gmail.com>
2026-07-26 22:12:54 +0700

    Set androidboot.verifiedbootstate=orange to allow adb remount
    adb remount and adb disable-verity require ro.boot.verifiedbootstate
    to equal "orange" (bootloader unlocked), otherwise fs_mgr_remount
    refuses with "Device must be bootloader unlocked" even on userdebug
    builds. This board has no real bootloader/AVB enforcement
    (TARGET_NO_BOOTLOADER := true), so the property was never set and
    adb remount always failed.
    
    Same fix as applied upstream for hikey/hikey960:
    https://android.googlesource.com/device/linaro/hikey/+/fa66979a87e53d92dc717fd524f7958af20eb9f3
    
    Tested on real Raspberry Pi 4 hardware: after this change,
    ro.boot.verifiedbootstate=orange is reported and
    "adb root && adb remount" succeeds (remounts / and /vendor as RW).

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_SCREEN_DENSITY := 240
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyAMA10,115200 androidboot.hardware=rpi5
+BOARD_KERNEL_CMDLINE := console=ttyAMA10,115200 androidboot.hardware=rpi5 androidboot.verifiedbootstate=orange
 
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
